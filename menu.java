a2feb00d8b38d8e7949969abb9f7d649a32e0912
rikzaramadhan <rikzaoke2@gmail.com>
2021-01-14 12:02:07 +0700

    belum jadi

---
+++ menu.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Asus
+ */
+public class menu {
+    private String nama;
+    private String jenis_kendaraan;
+    private String harga;
+    private String diskon;
+    private String tujuan;
+    
+    
+    public menu() {
+
+    }
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the id to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the kereta api
+     */
+    public String getJenis_kendaraan() {
+        return jenis_kendaraan;
+    }
+
+    /**
+     * @param jenis_kendaraan api the jenis_kendaraan api to set
+     */
+    public void setJenis_kendaraan(String jenis_kendaraan) {
+        this.jenis_kendaraan = jenis_kendaraan;
+    }
+
+    /**
+     * @return the harga
+     */
+    public String getHarga() {
+        return harga;
+    }
+
+    /**
+     * @param harga the harga to set
+     */
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    /**
+     * @return the diskon
+     */
+    public String getDiskon() {
+        return diskon;
+    }
+
+    /**
+     * @param diskon the diskon to set
+     */
+    public void setDiskon(String diskon) {
+        this.diskon = diskon;
+    }
+    /**
+     * @return the tujuan
+     */
+    public String getTujuan() {
+        return tujuan;
+    }
+
+    /**
+     * @param tujuan api the tujuan to set
+     */
+    public void setTujuan(String Tujuan) {
+        this.tujuan= tujuan;
+    }
+
+
+}
+
+
